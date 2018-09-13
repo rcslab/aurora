d64e0a58cdfa394d7ad003840e59e34d71314fbe
Emil Tsalapatis <emil.tsalapatis@uwaterloo.ca>
2018-09-13 05:30:50 -0400

    updated root test Makefile and added register test

---
+++ tests/register/register.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdint.h>
+
+#define CYCLES (1000 * 1000 * 1000)
+
+
+int main() 
+{
+	volatile register uint64_t rax asm ("rax");
+	volatile register uint64_t rbx asm ("rbx");
+	volatile register uint64_t rcx asm ("rcx");
+	volatile register uint64_t rdx asm ("rdx");
+
+	/* XXX We cannot use these registers it seems.
+	volatile register uint16_t cs asm ("cs");
+	volatile register uint16_t ds asm ("ds");
+	volatile register uint16_t es asm ("es");
+	volatile register uint16_t fs asm ("fs");
+	volatile register uint16_t gs asm ("gs");
+	volatile register uint16_t ss asm ("ss");
+	*/
+
+	int i;
+	
+	for (;;) {
+		rax = rbx = rcx = rdx = 0;
+
+		for (i = 0; i < CYCLES; i++) {
+			rax += 1;
+			rbx += 2;
+			rcx += 3;
+			rdx += 4;
+		}
+
+
+		printf("rax %lu\trbx %lu\trax %lu\trdx %lu\t\n", rax, rbx, rcx, rdx);
+		/*
+		printf("cs %d\tds %d\tes %d\tfs %d\tgs %d\tss %d\t\n", cs, ds, es, fs, gs, ss);
+		*/
+	}
+
+	return 0;
+}
