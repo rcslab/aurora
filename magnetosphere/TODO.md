e658ecd26b6688296222ac3b176c01ace0600607
Aimilios Tsalapatis <atsalapa@uwaterloo.ca>
2022-11-16 10:53:00 -0500

    removed obsolete benchmarks and DTrace scripts
    Summary: The tree includes obsolete benchmarks and DTrace scripts. Remove them.
    
    Test Plan: TBD
    
    Reviewers: ali, tavianator, ryan
    
    Reviewed By: ryan
    
    Differential Revision: https://review.rcs.uwaterloo.ca/D719

--- magnetosphere/TODO.md
+++
@@ -1,2 +0,0 @@
-Extract common functionality out of the scripts, which have been created by 
-copy pasting the original Redis one anyway.
--- scripts/wrk.sh
+++
@@ -1,3 +0,0 @@
-function wrkbench {
-    ssh -p "$WRKPORT" -i "$WRKSSH" "$WRKUSER@$WRKREMOTE" wrk -d "$WRKTIME" -t "$WRKTHREAD" -c "$WRKCONNS" "$WRKURL/$WRKFILE"
-}
