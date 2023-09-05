rcslab/aurora

commit 31cb1220d7daae060a1f5a13d928d23237622c4a

use the optimized sckpt allocation path everywhere
Summary: Allocating an slsckpt_data structure is costly, but partitions require at most two such data structures at any given time. Add code to reuse the slsckpt_data structure after finishing a checkpoint to avoid allocating new instances.

Test Plan:
Build: FreeBSD 12.3-RELEASE-p5 SLOWDBG
Host: aurora
Test started: Fri Aug 25 18:50:55 EDT 2023
Stress testing: OFF
Test                                       Status      Time
-----------------------------------------------------------
201-simple-mem-ckpt.sh                   [ success ]      2
202-simple-osd-ckpt.sh                   [ success ]      1
203-busy-loop.sh                         [ success ]      2
204-print-stdout.sh                      [ success ]      4
205-array-traversal.sh                   [ success ]      3
206-mmap-file.sh                         [ success ]      3
207-mmap-anonymous.sh                    [ success ]      3
208-signal-handler.sh                    [ success ]      4
209-pthreads-mutex.sh                    [ success ]      3
210-file-vnode.sh                        [ success ]     10
211-fork-simple.sh                       [ success ]      5
212-file-fifo.sh                         [ success ]      4
213-fork-shm.sh                          [ success ]     28
214-file-pipes.sh                        [ success ]      3
215-file-posixshm.sh                     [ success ]      4
216-sysv-shm.sh                          [ success ]      3
217-file-udplisten.sh                    [ success ]      4
218-file-tcplisten.sh                    [ success ]     13
219-kqueue.sh                            [ success ]      4
220-pgroups.sh                           [ success ]     21
221-lazy-osd-ckpt.sh                     [ success ]      1
222-self-ckpt-mem.sh                     [ success ]      5
223-restore-mem-multiple.sh              [ success ]      2
224-restore-osd-multiple.sh              [ success ]      2

sls/sls_partition.c
@@ -134,22 +134,28 @@ slsckpt_clear(struct slsckpt_data *sckpt)
 	sbuf_clear(sckpt->sckpt_meta);
 }
 
-struct slsckpt_data *
-slsckpt_alloc(struct sls_attr *attr)
+int
+slsckpt_alloc(struct slspart *slsp, struct slsckpt_data **sckptp)
 {
 	struct slsckpt_data *sckpt;
 	int error;
 
-	sckpt = malloc(sizeof(*sckpt), M_SLSMM, M_WAITOK | M_ZERO);
+	sckpt = slsp->slsp_blanksckpt;
+	slsp->slsp_blanksckpt = NULL;
+
+	if (sckpt == NULL)
+		sckpt = malloc(sizeof(*sckpt), M_SLSMM, M_WAITOK | M_ZERO);
+
 	error = slsckpt_init(sckpt);
 	if (error != 0) {
 		free(sckpt, M_SLSMM);
-		return (0);
+		return (error);
 	}
 
-	memcpy(&sckpt->sckpt_attr, attr, sizeof(sckpt->sckpt_attr));
+	memcpy(&sckpt->sckpt_attr, &slsp->slsp_attr, sizeof(sckpt->sckpt_attr));
+	*sckptp = sckpt;
 
-	return (sckpt);
+	return (0);
 }
 
 void
@@ -165,6 +171,7 @@ slsckpt_drop(struct slsckpt_data *sckpt)
 	bool release;
 
 	KASSERT(sckpt->sckpt_refcount > 0, ("dropping unreferenced sckpt"));
+
 	release = refcount_release(&sckpt->sckpt_refcount);
 
 	/* Free if that was the last reference. */

sls/sls_ckpt.c
@@ -223,7 +223,8 @@ slsckpt_compact_single(struct slspart *slsp, struct slsckpt_data *sckpt)
 	/* Collapse the new table into the old one. */
 	slsvm_objtable_collapsenew(
 	    slsp->slsp_sckpt->sckpt_shadowtable, sckpt->sckpt_shadowtable);
-	slsckpt_drop(sckpt);
+	slsckpt_clear(sckpt);
+	slsp->slsp_blanksckpt = sckpt;
 }
 
 void
@@ -390,13 +391,10 @@ static int __attribute__((noinline)) sls_ckpt(slsset *procset,
 #ifdef KTR
 	KVSET_FOREACH(procset, iter, p) { slsvm_print_vmspace(p->p_vmspace); }
 #endif
-	sckpt = slsp->slsp_blanksckpt;
-	slsp->slsp_blanksckpt = NULL;
 
-	if (sckpt == NULL)
-		sckpt = slsckpt_alloc(&slsp->slsp_attr);
-	if (sckpt == NULL)
-		return (ENOMEM);
+	error = slsckpt_alloc(slsp, &sckpt);
+	if (error != 0)
+		return (error);
 
 	SDT_PROBE0(sls, , , meta_start);
 	SDT_PROBE1(sls, , sls_ckpt, , "Creating the checkpoint");
@@ -721,14 +719,9 @@ slsckpt_dataregion(struct slspart *slsp, struct proc *p, vm_ooffset_t addr,
 		goto error_single;
 	}
 
-	sckpt = slsckpt_alloc(&slsp->slsp_attr);
-	if (sckpt == NULL) {
-		error = ENOMEM;
-		goto error_single;
-	}
-
-	SDT_PROBE0(sls, , , stopclock_start);
-	SDT_PROBE0(sls, , , meta_start);
+	error = slsckpt_alloc(slsp, &sckpt);
+	if (error != 0)
+		goto error;
 
 	SDT_PROBE1(sls, , slsckpt_dataregion, , "Getting the partition");
 

sls/sls_filebackend.c
@@ -494,9 +494,9 @@ sls_read_file(struct slspart *slsp, struct slsckpt_data **sckptp,
 	int error;
 	int ret;
 
-	sckpt = slsckpt_alloc(&slsp->slsp_attr);
-	if (sckpt == NULL)
-		return (ENOMEM);
+	error = slsckpt_alloc(slsp, &sckpt);
+	if (error != 0)
+		return (error);
 
 	SDT_PROBE1(sls, , sls_rest, , "Allocating checkpoint");
 

sls/sls_partition.h
@@ -119,7 +119,7 @@ bool slsp_restorable(struct slspart *slsp);
 #define SLSP_NOCKPT(slsp) (SLSATTR_ISNOCKPT((slsp->slsp_attr)))
 
 extern uma_zone_t slsckpt_zone;
-struct slsckpt_data *slsckpt_alloc(struct sls_attr *attr);
+int slsckpt_alloc(struct slspart *slsp, struct slsckpt_data **sckptp);
 void slsckpt_clear(struct slsckpt_data *sckpt);
 void slsckpt_hold(struct slsckpt_data *sckpt);
 void slsckpt_drop(struct slsckpt_data *sckpt);

sls/sls_sockrcv.c
@@ -48,10 +48,11 @@ static int
 slsrcvd_ckptstart(struct sls_sockrcvd_state *rcvd, struct slsmsg_ckptstart *msg)
 {
 	struct slsckpt_data *sckpt;
+	int error;
 
-	sckpt = slsckpt_alloc(&rcvd->slsrcvd_slsp->slsp_attr);
-	if (sckpt == NULL)
-		return (ENOMEM);
+	error = slsckpt_alloc(rcvd->slsrcvd_slsp, &sckpt);
+	if (error != 0)
+		return (error);
 
 	rcvd->slsrcvd_sckpt = sckpt;
 	rcvd->slsrcvd_epoch = msg->slsmsg_epoch;

sls/sls_table.c
@@ -899,9 +899,9 @@ sls_read_slos(struct slspart *slsp, struct slsckpt_data **sckptp,
 	size_t buflen;
 	int error;
 
-	sckpt = slsckpt_alloc(&slsp->slsp_attr);
-	if (sckpt == NULL)
-		return (ENOMEM);
+	error = slsckpt_alloc(slsp, &sckpt);
+	if (error != 0)
+		return (error);
 
 	SDT_PROBE1(sls, , sls_rest, , "Allocating checkpoint");
 

commit 5c626f223bd060a2c763f56be2bf4aa4a0011ec9

plug memory leak introduced by 31cb1220d7da
Summary: Commit 31cb1220d7da introduced memory leaks by allocating key-value tables for already initialized fields in struct slsckpt_data. Plug the leaks by not calling slsckpt_init() when reusing a struct slsckpt_data during slsckpt_alloc().

Test Plan:
Build: FreeBSD 12.3-RELEASE-p5 SLOWDBG
Host: aurora
Test started: Mon Sep 4 10:33:32 EDT 2023
Stress testing: OFF
Test                                       Status      Time
-----------------------------------------------------------
201-simple-mem-ckpt.sh                   [ success ]      1
202-simple-osd-ckpt.sh                   [ success ]      2
203-busy-loop.sh                         [ success ]      2
204-print-stdout.sh                      [ success ]      3
205-array-traversal.sh                   [ success ]      3
206-mmap-file.sh                         [ success ]      3
207-mmap-anonymous.sh                    [ success ]      2
208-signal-handler.sh                    [ success ]      5
209-pthreads-mutex.sh                    [ success ]      3
210-file-vnode.sh                        [ success ]     11
211-fork-simple.sh                       [ success ]      4
212-file-fifo.sh                         [ success ]      4
213-fork-shm.sh                          [ success ]     21
214-file-pipes.sh                        [ success ]      3
215-file-posixshm.sh                     [ success ]      3
216-sysv-shm.sh                          [ success ]      4
217-file-udplisten.sh                    [ success ]      4
218-file-tcplisten.sh                    [ success ]     13
219-kqueue.sh                            [ success ]      4
220-pgroups.sh                           [ success ]     21
221-lazy-osd-ckpt.sh                     [ success ]      2
222-self-ckpt-mem.sh                     [ success ]      4
223-restore-mem-multiple.sh              [ success ]      2
224-restore-osd-multiple.sh              [ success ]      2

sls/sls_partition.c
@@ -143,13 +143,14 @@ slsckpt_alloc(struct slspart *slsp, struct slsckpt_data **sckptp)
 	sckpt = slsp->slsp_blanksckpt;
 	slsp->slsp_blanksckpt = NULL;
 
-	if (sckpt == NULL)
+	if (sckpt == NULL) {
 		sckpt = malloc(sizeof(*sckpt), M_SLSMM, M_WAITOK | M_ZERO);
 
-	error = slsckpt_init(sckpt);
-	if (error != 0) {
-		free(sckpt, M_SLSMM);
-		return (error);
+		error = slsckpt_init(sckpt);
+		if (error != 0) {
+			free(sckpt, M_SLSMM);
+			return (error);
+		}
 	}
 
 	memcpy(&sckpt->sckpt_attr, &slsp->slsp_attr, sizeof(sckpt->sckpt_attr));